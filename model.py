0b2e9f6d874a51eef244d5315f10be9f9188c967
a.dergachev <bestacc@mail.ru>
2023-02-24 16:22:00 +0300

    modified class Note, append to_dict func

--- model.py
+++ model.py
@@ -1,14 +1,23 @@
-from datetime import datetime
 import uuid
+from datetime import datetime
 
 
 class Note:
 
     def __init__(self, title: str, content: str):
-        self.id = str(uuid.uuid4())
-        self._heading = title
-        self._body = content
-        self._time_stamp = datetime
+        self._id = str(uuid.uuid4())
+        self._title = title
+        self._content = content
+        self._time_stamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def to_dict(self):
+        return {
+            'id': self._id,
+            'title': self._title,
+            'content': self._content,
+            'time_stamp': self._time_stamp
+        }
+
 
 
 
